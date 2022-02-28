30956ca91b3e4754271fecfbbc299084015151fa
rumeysa-akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-02-28 14:55:32 +0300

    Add project files.

---
+++ GymManagement.Domain/Entities/Member.cs
@@ -0,0 +1,16 @@
+﻿namespace GymManagement.Domain.Entities
+{
+    public class Member:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public double Dept { get; set; }
+        public bool IsPaymentStatus { get; set; }
+        public ExerciseProgram exerciseProgram { get; set; }
+        public int CampaignId { get; set; }
+       
+        //public int MyProperty { get; set; }
+
+    }
+}
---
+++ GymManagement.Domain/Entities/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.Domain.Entities
+{
+    public class Worker :Employee
+    {
+       
+        public int WorkerContractId { get; set; }
+        public WorkerContract  WorkerContract { get; set; }
+        public List<Mission> Missions { get; set; }
+    }
+}
