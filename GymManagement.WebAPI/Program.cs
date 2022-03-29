c9dd694976cf19edaa173ccb8f08a7ba9f41c065
Rumeysa Akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-03-29 04:33:54 +0300

    Update Program.cs

--- GymManagement.WebAPI/Program.cs
+++ GymManagement.WebAPI/Program.cs
@@ -20,7 +20,7 @@ namespace GymManagement.WebAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();//eksik
                 });
     }
 }
